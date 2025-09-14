ee5a5a6239b50739fc560570964ade510da58be3
Nathan Garside <git@nathangarside.com>
2025-09-14 11:22:58 +0100

    Add org helper

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,24 @@
 
 import { Hono } from 'hono'
 
+const org = 'https://github.com/pixelatedlabs'
+
 const build = (repo: string) => new Hono()
-	.all(`/`, c => c.redirect(`https://github.com/pixelatedlabs/${repo}`, 307))
-	.all(`/license`, c => c.redirect(`https://github.com/pixelatedlabs/${repo}/blob/master/license.txt`, 307))
-	.all(`/releases`, c => c.redirect(`https://github.com/pixelatedlabs/${repo}/releases`, 307))
-	.all(`/releases/latest`, c => c.redirect(`https://github.com/pixelatedlabs/${repo}/releases/latest`, 307))
+	.all(`/`, c => c.redirect(`${org}/${repo}`, 307))
+	.all(`/license`, c => c.redirect(`${org}/${repo}/blob/master/license.txt`, 307))
+	.all(`/releases`, c => c.redirect(`${org}/${repo}/releases`, 307))
+	.all(`/releases/latest`, c => c.redirect(`${org}/${repo}/releases/latest`, 307))
 	.all(`/releases/latest/:asset`, c => {
 		const { asset } = c.req.param()
-		return c.redirect(`https://github.com/pixelatedlabs/${repo}/releases/latest/download/${asset}`, 307)
+		return c.redirect(`${org}/${repo}/releases/latest/download/${asset}`, 307)
 	})
 	.all(`/releases/:tag`, c => {
 		const { tag } = c.req.param()
-		return c.redirect(`https://github.com/pixelatedlabs/${repo}/releases/${tag}`, 307)
+		return c.redirect(`${org}/${repo}/releases/${tag}`, 307)
 	})
 	.all(`/releases/:tag/:asset`, c => {
 		const { asset, tag } = c.req.param()
-		return c.redirect(`https://github.com/pixelatedlabs/${repo}/releases/download/${tag}/${asset}`, 307)
+		return c.redirect(`${org}/${repo}/releases/download/${tag}/${asset}`, 307)
 	})
 
 export default new Hono()
