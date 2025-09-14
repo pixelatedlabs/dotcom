8bcee03f04d65d5f66b978914701ee15cb14faa2
Nathan Garside <git@nathangarside.com>
2025-09-14 11:09:12 +0100

    Add redirect

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
+	.all('headcheck', (c) => c.redirect('https://github.com/pixelatedlabs/headcheck', 307))
 	.all('*', () => new Response(undefined, { status: 427 }))
 
 export default app
