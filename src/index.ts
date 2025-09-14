5d5bc4f7ce24bd851e8b90b5addfa1b92184a3d6
Nathan Garside <git@nathangarside.com>
2025-09-14 11:24:35 +0100

    Use 302 redirects

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,21 @@ import { Hono } from 'hono'
 const org = 'https://github.com/pixelatedlabs'
 
 const build = (repo: string) => new Hono()
-	.all(`/`, c => c.redirect(`${org}/${repo}`, 307))
-	.all(`/license`, c => c.redirect(`${org}/${repo}/blob/master/license.txt`, 307))
-	.all(`/releases`, c => c.redirect(`${org}/${repo}/releases`, 307))
-	.all(`/releases/latest`, c => c.redirect(`${org}/${repo}/releases/latest`, 307))
+	.all(`/`, c => c.redirect(`${org}/${repo}`))
+	.all(`/license`, c => c.redirect(`${org}/${repo}/blob/master/license.txt`))
+	.all(`/releases`, c => c.redirect(`${org}/${repo}/releases`))
+	.all(`/releases/latest`, c => c.redirect(`${org}/${repo}/releases/latest`))
 	.all(`/releases/latest/:asset`, c => {
 		const { asset } = c.req.param()
-		return c.redirect(`${org}/${repo}/releases/latest/download/${asset}`, 307)
+		return c.redirect(`${org}/${repo}/releases/latest/download/${asset}`)
 	})
 	.all(`/releases/:tag`, c => {
 		const { tag } = c.req.param()
-		return c.redirect(`${org}/${repo}/releases/${tag}`, 307)
+		return c.redirect(`${org}/${repo}/releases/${tag}`)
 	})
 	.all(`/releases/:tag/:asset`, c => {
 		const { asset, tag } = c.req.param()
-		return c.redirect(`${org}/${repo}/releases/download/${tag}/${asset}`, 307)
+		return c.redirect(`${org}/${repo}/releases/download/${tag}/${asset}`)
 	})
 
 export default new Hono()
